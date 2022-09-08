7c1c49fb2254191005ad8bfa5f6e1194dfbbb301
alonpatashnik <alonp714@gmail.com>
2022-09-08 12:18:37 -0700

    added project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -21,6 +21,11 @@ export default function PortfolioSection() {
 
 
   const Card = (props) => {
+    let secondLink = ""
+    if (props.github2) {
+      secondLink = <a class="card-text" href={props.github2}>Second Github URL</a>
+    }
+    
     return(
       <div class="card">
           <img src={props.img} class="card-img-top image" alt="app screenshot"/>
@@ -29,6 +34,8 @@ export default function PortfolioSection() {
             <a class="card-text" href={props.deploy} >Deployed URL</a>
             <br/>
             <a class="card-text" href={props.github} >Github URL</a>
+            <br/>
+            {secondLink}
           </div>
       </div>
     )
@@ -39,6 +46,7 @@ export default function PortfolioSection() {
      <Card img="/cute-cuddly.png" title="Cute And Cuddly" deploy="https://gabrielwarner.github.io/cute-and-cuddly/" github="https://github.com/GabrielWarner/cute-and-cuddly"/>
      <Card img="/play-outside.png" title="Play Outside" deploy="https://play-outside.herokuapp.com/" github="https://github.com/alonpatashnik/project-two"/>
      <Card img="/word-check.png" title="Wordcheck" deploy="https://github.com/alonpatashnik/word-check-test" github="https://github.com/alonpatashnik/word-check-test"/>
+     <Card img="/HolloeReach.png" title="The Hollow Reach" deploy="https://funny-meerkat-dcfce3.netlify.app/" github="https://github.com/alonpatashnik/rpg-word-game-frontend" github2="https://github.com/alonpatashnik/rpg-word-game-backend"/>
     </div>
 );
 }
